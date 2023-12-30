3805909b1b109d736ed1b3a742f0bf0b41d5efa5
cod1ngWizard <asidedshadow@gmail.com>
2023-12-29 18:20:44 -0600

    Removes middleware params

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs";
  
-export default authMiddleware({
-  publicRoutes: ['/', '/api/webhook/clerk'],
-  ignoredRoutes: ['/api/webhook/clerk', '/*']
-});
+export default authMiddleware();
  
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
