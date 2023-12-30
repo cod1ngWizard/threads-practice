5cf0483ee512adaa053d4d54a040ef09dc6f6550
cod1ngWizard <asidedshadow@gmail.com>
2023-12-29 18:16:32 -0600

    Removes space

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,10 @@
-import { authMiddleware } from '@clerk/nextjs';
-
+import { authMiddleware } from "@clerk/nextjs";
+ 
 export default authMiddleware({
   publicRoutes: ['/', '/api/webhook/clerk'],
-  ignoredRoutes: ['/api/webhook/clerk', '/*'],
+  ignoredRoutes: ['/api/webhook/clerk', '/*']
 });
-
+ 
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
