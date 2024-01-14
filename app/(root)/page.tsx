cff7c0f37fd461ea74e264d6be61cd474b842947
cod1ngWizard <asidedshadow@gmail.com>
2024-01-13 23:38:46 -0600

    Redirects to onboarding if not user info

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,7 @@ import ThreadCard from '@/components/cards/ThreadCard';
 import { fetchPosts } from '@/lib/actions/thread.actions';
 import { fetchUser } from '@/lib/actions/user.actions';
 import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 
 export default async function Home() {
   const retrievedPosts: any = await fetchPosts();
@@ -12,6 +13,10 @@ export default async function Home() {
   }
 
   const userInfo = await fetchUser(user.id);
+
+  if (!userInfo) {
+    redirect('/onboarding');
+  }
 
   return (
     <>
